b51194bbe0577d5c50de6c0b614e7c051b6acf7d
GhAli21 <jhtest.dev21@gmail.com>
2026-03-17 06:20:33 +0400

    16_03_2026_5 fix enhance new order page
    Files: web-admin/src/features/orders/ui/product-grid.tsx

--- web-admin/src/features/orders/ui/product-grid.tsx
+++ web-admin/src/features/orders/ui/product-grid.tsx
@@ -174,7 +174,6 @@ export const ProductGrid = memo(function ProductGrid({
         onConditionToggle={onConditionToggle}
         enforcePrefCompatibility={enforcePrefCompatibility}
       />
-      </div>
     </div>
   );
 });
